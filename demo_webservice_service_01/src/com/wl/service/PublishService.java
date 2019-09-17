ce6373789442491f97f2eecb5f2e0cfc8bae7549
15112125687@163.com <15171377969wl>
2019-09-17 20:55:37 +0800

    删除无用代码

--- demo_webservice_service_01/src/com/wl/service/PublishService.java
+++
@@ -1,9 +0,0 @@
-package com.wl.service;
-
-import javax.xml.ws.Endpoint;
-
-public class PublishService {
-	public static void main(String[] args) {
-		Endpoint.publish("http://localhost:9999/hello", new HelloServiceImpl());
-	}
-}
